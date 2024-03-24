679a0a0235b6fae9266277636c48968a7feac0c8
gopal172002 <gopalkalsiya1h@gmail.com>
2024-03-24 21:51:43 +0530

    template

---
+++ gopp/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      {/* Add your components and JSX here */}
+    </div>
+  );
+}
+
+export default App;
---
+++ gopp/src/other.js
@@ -0,0 +1,6 @@
+const someFunction = () => {
+    return 'This is a function from other.js';
+  }
+  
+  export default someFunction;
+  
